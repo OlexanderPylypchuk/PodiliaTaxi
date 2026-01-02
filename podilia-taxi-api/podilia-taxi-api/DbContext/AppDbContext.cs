0d36fe1c4358ad998f8d5033452499afa53b98a7
Oleksandr Pylypchuk <oleksandr.pylypchuk@intenselab.eu>
2026-01-02 22:17:58 +0200

    DbContext

---
+++ podilia-taxi-api/podilia-taxi-api/DbContext/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace podilia_taxi_api.DbContext
+{
+    public class AppDbContext : IdentityDbContext<User>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+    }
+}
---
+++ podilia-taxi-api/podilia-taxi-api/User.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace podilia_taxi_api
+{
+    public class User : IdentityUser
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public double Rating { get; set; }
+    }
+}
