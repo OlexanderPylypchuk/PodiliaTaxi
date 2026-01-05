6ca6da7283a3eb8746325977b6e6637411ab1bdb
Oleksandr Pylypchuk <oleksandr.pylypchuk@intenselab.eu>
2026-01-05 21:39:17 +0200

    UnitOfWork

---
+++ podilia-taxi-api/podilia-taxi-api/DbContext/Repository/IRepository/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿namespace podilia_taxi_api.DbContext.Repository.IRepository
+{
+    public interface IUnitOfWork
+    {
+        IUserRepository Users { get; }
+        IRideRepository Rides { get; }
+        IRideOrderRepository RideOrders { get; }
+
+
+    }
+}
---
+++ podilia-taxi-api/podilia-taxi-api/DbContext/Repository/UnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using podilia_taxi_api.DbContext.Repository.IRepository;
+
+namespace podilia_taxi_api.DbContext.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public IUserRepository Users { get; }
+
+        public IRideRepository Rides { get; }
+
+        public IRideOrderRepository RideOrders { get; }
+
+        public UnitOfWork(AppDbContext db)
+        {
+            Users = new UserRepository(db);
+            Rides = new RideRepository(db);
+            RideOrders = new RideOrderRepository(db);
+        }
+    }
+}
